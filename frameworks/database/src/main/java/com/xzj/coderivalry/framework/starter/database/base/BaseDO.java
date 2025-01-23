ea39918965fd5fdafbc287ffa02c5b585b40bb26
AA <1435220156@qq.com>
2025-01-23 21:15:36 +0800

    feat: 新增基础属性类

---
+++ frameworks/database/src/main/java/com/xzj/coderivalry/framework/starter/database/base/BaseDO.java
@@ -0,0 +1,23 @@
+package com.xzj.coderivalry.framework.starter.database.base;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * 数据库持久层基础属性
+ * @author xzj
+ */
+@Data
+public class BaseDO {
+
+    // 创建时间
+    @TableField(fill = FieldFill.INSERT)
+    private Date createTime;
+
+    // 删除标识 0：未删除 1：已删除
+    @TableField(fill = FieldFill.INSERT)
+    private Integer delFlag;
+}
