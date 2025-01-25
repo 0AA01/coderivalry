4a09e54a94e90eb079ec07086679daf8c5ede191
AA <1435220156@qq.com>
2025-01-25 22:11:55 +0800

    feat:修改基础属性类

--- frameworks/database/src/main/java/com/xzj/coderivalry/framework/starter/database/base/BaseDO.java
+++ frameworks/database/src/main/java/com/xzj/coderivalry/framework/starter/database/base/BaseDO.java
@@ -13,11 +13,16 @@ import java.util.Date;
 @Data
 public class BaseDO {
 
-    // 创建时间
+    /**
+     * 创建时间
+     */
     @TableField(fill = FieldFill.INSERT)
     private Date createTime;
 
-    // 删除标识 0：未删除 1：已删除
+    /**
+     * 删除标识
+     * 0：未删除 1：已删除
+     */
     @TableField(fill = FieldFill.INSERT)
     private Integer delFlag;
 }
