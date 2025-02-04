f55de36c27c958a4224ac56c16a03791f3a0f539
AA <1435220156@qq.com>
2025-02-04 17:41:14 +0800

    feat:新增spring启动容器

---
+++ frameworks/base/src/main/java/com/xzj/coderivalry/framework/base/starter/bases/ApplicationContextHolder.java
@@ -0,0 +1,56 @@
+package com.xzj.coderivalry.framework.base.starter.bases;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+import java.lang.annotation.Annotation;
+import java.util.Map;
+
+/**
+ * Application context holder
+ */
+public class ApplicationContextHolder implements ApplicationContextAware {
+
+    private static ApplicationContext CONTEXT;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        ApplicationContextHolder.CONTEXT = applicationContext;
+    }
+
+    /**
+     * 按照类型获取Bean
+     */
+    public static <T> T getBean(Class<T> clazz) {
+        return CONTEXT.getBean(clazz);
+    }
+
+    /**
+     * 通过名称和类型获取Bean
+     */
+    public static <T> T getBean(String name, Class<T> clazz) {
+        return CONTEXT.getBean(name, clazz);
+    }
+
+    /**
+     * 按照类型获取一组Bean
+     */
+    public static <T> Map<String, T> getBeansOfType(Class<T> clazz) {
+        return CONTEXT.getBeansOfType(clazz);
+    }
+
+    /**
+     * 根据名称和注解获取Bean
+     */
+    public static <A extends Annotation> A findAnnotationOnBean(String beanName, Class<A> annotationType) {
+        return CONTEXT.findAnnotationOnBean(beanName, annotationType);
+    }
+
+    /**
+     * 获取 ApplicationContext
+     */
+    public static ApplicationContext getInstance() {
+        return CONTEXT;
+    }
+}
