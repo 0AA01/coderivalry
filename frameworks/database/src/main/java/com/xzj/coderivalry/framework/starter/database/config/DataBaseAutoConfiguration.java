7b957491d7211cdc32479c4d7e1fa7b602fc6ad8
AA <1435220156@qq.com>
2025-02-04 17:47:26 +0800

    feat:新增mybatis-plus分页插件

--- frameworks/database/src/main/java/com/xzj/coderivalry/framework/starter/database/config/DataBaseAutoConfiguration.java
+++ frameworks/database/src/main/java/com/xzj/coderivalry/framework/starter/database/config/DataBaseAutoConfiguration.java
@@ -1,5 +1,8 @@
 package com.xzj.coderivalry.framework.starter.database.config;
 
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import com.xzj.coderivalry.framework.starter.database.handler.MyMetaObjectHandler;
 import org.springframework.context.annotation.Bean;
 
@@ -14,5 +17,16 @@ public class DataBaseAutoConfiguration {
     @Bean
     public MyMetaObjectHandler myMetaObjectHandler() {
         return new MyMetaObjectHandler();
+    }
+
+
+    /**
+     * 分页插件
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
     }
 }
