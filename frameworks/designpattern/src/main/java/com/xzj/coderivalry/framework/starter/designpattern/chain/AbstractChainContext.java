bc58604fefd18f13bfe6b0d572052007a789de5c
AA <1435220156@qq.com>
2025-02-04 09:57:19 +0800

    feat:新增designpatternStarter模块，责任链功能

---
+++ frameworks/designpattern/src/main/java/com/xzj/coderivalry/framework/starter/designpattern/chain/AbstractChainContext.java
@@ -0,0 +1,43 @@
+package com.xzj.coderivalry.framework.starter.designpattern.chain;
+
+import com.xzj.coderivalry.framework.base.starter.bases.ApplicationContextHolder;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.Ordered;
+import org.springframework.util.CollectionUtils;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * 抽象责任链上下文
+ */
+public class AbstractChainContext<T> implements CommandLineRunner {
+
+    private final Map<String, List<AbstractChainHandler>> abstractChainHandlerContainer = new HashMap<>();
+
+    public void handler(String mark, T requestParam) {
+        List<AbstractChainHandler> abstractChainHandlers = abstractChainHandlerContainer.get(mark);
+        if (CollectionUtils.isEmpty(abstractChainHandlers)) {
+            throw new RuntimeException(String.format("[%s] Chain of Responsibility ID is undefined.", mark));
+        }
+        abstractChainHandlers.forEach(each -> each.handler(requestParam));
+    }
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        Map<String, AbstractChainHandler> chainFilterMap = ApplicationContextHolder
+                .getBeansOfType(AbstractChainHandler.class);
+        chainFilterMap.forEach((beanName, bean) -> {
+            List<AbstractChainHandler> abstractChainHandlers = abstractChainHandlerContainer.get(bean.mark());
+            if (CollectionUtils.isEmpty(abstractChainHandlers)) {
+                abstractChainHandlers = new ArrayList();
+            }
+            abstractChainHandlers.add(bean);
+            List<AbstractChainHandler> actualAbstractChainHandlers = abstractChainHandlers.stream()
+                    .sorted(Comparator.comparing(Ordered::getOrder))
+                    .collect(Collectors.toList());
+            abstractChainHandlerContainer.put(bean.mark(), actualAbstractChainHandlers);
+        });
+    }
+}
